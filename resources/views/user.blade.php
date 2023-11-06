bab5cb6d48b49c7e4a0288e4944954476d1c945e
4KRO5 <4kro54k@gmail.com>
2023-11-05 21:50:00 -0700

    Creación de rutas en método GET

---
+++ resources/views/user.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Usuario</title>
+</head>
+
+<body>
+    <h1>!Bienvenido, {{ $nombre }}!</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente repellendus facere officiis doloremque quos placeat,
+        maxime dolorum libero saepe optio distinctio eveniet et fugit. Nisi hic facilis deserunt. Laboriosam, veritatis!
+    </p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/////////////////////////////////////// Operaciones ///////////////////////////////////////
+Route::pattern('num1', '[0-9]+');
+Route::pattern('num2', '[0-9]+');
+
+Route::get('/operaciones/{operacion}/{num1}/{num2}', function ($operacion, $num1, $num2) {
+    switch ($operacion) {
+        case 'suma':
+            $resultado = $num1 + $num2;
+            break;
+        case 'resta':
+            $resultado = $num1 - $num2;
+            break;
+        case 'multiplicacion':
+            $resultado = $num1 * $num2;
+            break;
+        case 'division':
+            if ($num2 != 0) {
+                $resultado = $num1 / $num2;
+            } else {
+                return "No se puede dividir entre cero";
+            }
+            break;
+        default:
+            return "Operación no válida";
+    }
+
+    return "El resultado de la operación $operacion es: $resultado";
+});
+
+/////////////////////////////////////// Saludar Usuario ///////////////////////////////////////
+Route::pattern('nombre', '[A-Za-zÀ-ÖØ-öø-ÿ\s]+');
+Route::pattern('apellido', '[A-Za-zÀ-ÖØ-öø-ÿ\s]+');
+
+Route::get('/saludo/{nombre}/{apellido?}', function ($nombre, $apellido = null) {
+    return "¡Hola $nombre" . ($apellido ? " $apellido" : "") . "!";
+});
+
+/////////////////////////////////////// Saludo Desde Vista ///////////////////////////////////////
+Route::get('/user/{nombre}', function ($nombre) {
+    return view('user', ['nombre' => $nombre]);
+});
